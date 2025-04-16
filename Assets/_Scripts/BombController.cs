f4a5c8a9ea4086ae4fb2556d02ca9e99ee63d3a4
khuu86 <tankhuu@gmail.com>
2025-04-16 19:34:36 +0200

    Explosion Script + Sprite
    Ikke færdig endnu

--- Assets/_Scripts/BombController.cs
+++ Assets/_Scripts/BombController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BombController : MonoBehaviour
@@ -8,6 +9,11 @@ public class BombController : MonoBehaviour
     public float bombFuseTime = 3f;
     public int bombAmount = 1;
     private int bombsRemaining;
+
+    // Eksplosionen er en prefab, der skal placeres i editoren
+    public Explosion explosionPrefab;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 1;
 
     private void OnEnable()
     {
@@ -35,10 +41,45 @@ public class BombController : MonoBehaviour
 
         yield return new WaitForSeconds(bombFuseTime); // venter i bombFuseTime sekunder
 
+        // Opdaterer bombens position til nærmeste heltal for at sikre, at den placeres præcist på gridden
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start); // sætter den aktive renderer til start spritet
+        explosion.DestroyAfter(explosionDuration); // sletter eksplosionsobjektet efter explosionDuration sekunder
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
+
         // Slettes senere 
         Destroy(bomb);
         bombsRemaining++;
     }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        // hvis længden er 0, gør ikke noget
+        if (length <= 0) { return; }
+
+        position += direction; // flytter positionen i den givne retning
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        if (length > 1)
+        { explosion.SetActiveRenderer(explosion.middle); }
+        else
+        { explosion.SetActiveRenderer(explosion.end); }
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, direction, length - 1);
+    }
+
 
     // Når spilleren bevæger sig væk fra bombens collider, udløses OnTriggerExit2D()
     private void OnTriggerExit2D(Collider2D other)
