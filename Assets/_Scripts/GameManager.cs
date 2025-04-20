61ca80a51c9516b14a0089e3128bf3be3a9ab118
khuu86 <tankhuu@gmail.com>
2025-04-20 17:51:34 +0200

    Tilføjet Game Manager
    Tilføjet Game Manager hvor den tjekker hvis der kun er 1 i live så genstarter den scenen efter 5 sekunder

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Bruger SceneManager til at genstarte scenen
+
+public class GameMAnager : MonoBehaviour 
+{
+    public GameObject[] players;
+    
+    public void CheckWinState()
+    {
+        int aliveCount = 0;
+
+        foreach (GameObject player in players)
+        {
+            if (player.activeSelf)
+            {
+                aliveCount++;
+            }
+        }
+        if (aliveCount <= 1)
+        {
+            Invoke(nameof(NewRound), 5f);
+        }
+    }
+
+    private void NewRound()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Genstarter den nuværende scene
+    }
+
+    // GameManager bliver kaldt i PlayerController, hvor den tjekker CheckWinState()
+}
